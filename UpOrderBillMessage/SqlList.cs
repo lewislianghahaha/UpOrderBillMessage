c256328d437a933b4eba81d1d8d1ec6143ea654c
Lewis <heshanpin2016@163.com>
2021-10-06 10:51:00 +0800

    20211006:SQL相关修改

--- UpOrderBillMessage/SqlList.cs
+++ UpOrderBillMessage/SqlList.cs
@@ -12,7 +12,14 @@
         public string GetUpdate(string listid)
         {
             _result = $@"
-                            
+                            UPDATE A SET A.F_YTC_TEXT41=X.开票信息名称
+                            FROM dbo.T_BD_CUSTOMER A
+                            INNER JOIN (
+				                            SELECT  TOP 1 B.F_YTC_BASE 客户ID,B.F_YTC_TEXT1 开票信息名称
+				                            FROM dbo.ytc_t_Cust100018 B   
+				                            ORDER BY B.FID DESC
+                                       )X ON A.FCUSTID=X.客户ID
+                            WHERE A.FCUSTID IN ({listid})
                         ";
 
             return _result;
